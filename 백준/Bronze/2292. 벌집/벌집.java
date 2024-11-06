93d81a3bd2f5ed6514ba27a446eb24c64993e7e9
hznnoy <152006906+hznnoy@users.noreply.github.com>
2024-11-06 21:25:57 +0900

    [Bronze II] Title: 벌집, Time: 104 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int a = 1;
+        int range = 2; 
+        if(n==1){
+            System.out.print(1);
+        }else{
+            while(range <= n){
+                range = range + (6 * a);
+                a++;
+            }
+            System.out.print(a);
+        } 
+    }
+}
