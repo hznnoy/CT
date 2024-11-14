7883f5a9117d930b621a00c374e85db0f9feab41
hznnoy <152006906+hznnoy@users.noreply.github.com>
2024-11-15 02:36:21 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+				
+		int[] t = new int[n];
+		int[] p = new int[n];
+		int[] dp = new int[n+1];
+		
+		for(int i=0; i<n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			t[i]= Integer.parseInt(st.nextToken());					
+			p[i]=Integer.parseInt(st.nextToken());
+		}
+
+		for(int i=0; i<n; i++) {
+			if(i+t[i]<=n) {
+				dp[i+t[i]] = Math.max(dp[i+t[i]], dp[i]+p[i]);
+			}
+			dp[i+1] = Math.max(dp[i+1], dp[i]); //오늘 일한 것 다음 날로 누적
+		}
+		System.out.print(dp[n]);
+	}
+}
