b3aaa05eaf4e89b07f63f902e85c59dd2799818a
hznnoy <152006906+hznnoy@users.noreply.github.com>
2025-03-05 17:21:22 +0900

    [level 1] Title: 최소직사각형, Time: 161.73 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -3,14 +3,30 @@ import java.util.*;
 class Solution {
     public int solution(int[][] sizes) {
         int answer = 0;
-        int w=0, h=0;
+        int temp = 0;
         
-        for(int[] wallet : sizes){
-            w = Math.max(w, Math.max(wallet[0], wallet[1]));
-            h = Math.max(h, Math.min(wallet[0], wallet[1]));
+        // 각 명함의 가로, 세로 길이 중 더 큰 쪽을 [i][i][0]으로 이동
+        for(int i=0; i<sizes.length; i++){ 
+            for(int j=0; j<sizes.length; j++){
+                if(sizes[i][0]<sizes[i][1]){
+                    temp = sizes[i][0];
+                    sizes[i][0] = sizes[i][1];
+                    sizes[i][1] = temp;
+                }
+            }
         }
         
-        answer = w*h;
+        Arrays.sort(sizes, (o1,o2) -> { // 가로 길이 내림차순 정렬
+            return o2[0]-o1[0];
+        });
+        int max_w = sizes[0][0];
+        
+        Arrays.sort(sizes, (o1,o2) -> { // 세로 길이 내림차순 정렬
+            return o2[1]-o1[1];
+        });
+        int max_h = sizes[0][1];
+        
+        answer = max_w * max_h; // 최댓값끼리 곱하여 return 
         return answer;
     }
 }
