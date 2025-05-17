3f6d243d91792842cac5c17c79b83718e5273d8e
hznnoy <152006906+hznnoy@users.noreply.github.com>
2025-05-17 22:49:55 +0900

    [Silver I] Title: 미로 탐색, Time: 116 ms, Memory: 14628 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    static int n, m;
+    static int[][] maze;
+    static boolean[][] visited;
+
+    static Queue<int[]> queue = new LinkedList<>();
+
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        maze = new int[n][m];
+        visited = new boolean[n][m];
+
+        for(int i=0;i<n; i++){
+            String line  = br.readLine();
+            for(int j=0; j<m; j++){
+                maze[i][j] = line.charAt(j)-'0';
+            }
+        }
+        bfs(0,0);
+        System.out.print(maze[n-1][m-1]);
+    }
+
+    static void bfs(int x, int y){
+        queue.add(new int[] {x,y});
+        visited[x][y]=true;
+
+        while(!queue.isEmpty()){
+            int[] temp = queue.poll();
+            int cx = temp[0];
+            int cy = temp[1];
+            for(int i=0; i<4; i++){
+                int nx = cx+dx[i];
+                int ny = cy+dy[i];
+
+                if(nx>=0 && ny>=0 && nx<n && ny<m){
+                    if(!visited[nx][ny] && maze[nx][ny]==1){
+                        visited[nx][ny]=true;
+                        maze[nx][ny]=maze[cx][cy]+1;
+                        queue.add(new int[] {nx,ny});
+                    }
+                }
+            }
+        }
+    }
+
+}
