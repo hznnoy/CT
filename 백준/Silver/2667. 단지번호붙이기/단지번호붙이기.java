a289b29e2d59f0e99ce73300d41397b781c5858d
hznnoy <152006906+hznnoy@users.noreply.github.com>
2025-05-18 00:21:59 +0900

    [Silver I] Title: 단지번호붙이기, Time: 104 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,60 +1,71 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main{
-	static int n;
-	static int[][] arr;
-	static int[] dx = {-1,1,0,0};
-	static int[] dy = {0,0,-1,1};
-	static boolean[][] visit;
-	static int count; // 단지 내 아파트 수
-	static int total = 0; // 단지 수
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
-		n = Integer.parseInt(br.readLine());
-		arr = new int[n][n];
-		visit = new boolean[n][n];
-		
-		for(int i=0; i<n; i++) {
-			String s = br.readLine();
-			for(int j=0; j<n; j++) {
-				arr[i][j] = s.charAt(j)-'0';
-			}
-		}
-		
-		List<Integer> counthouse = new ArrayList<>();
-		
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<n; j++) {
-				if(arr[i][j]==1 && !visit[i][j]) {
-					count=0;
-					total++;
-					find(i,j);
-					counthouse.add(count);
-				}
-			}
-		}
-		System.out.println(total);
-		Collections.sort(counthouse);
-		
-		for(int i=0; i<counthouse.size();i++) {
-			System.out.println(counthouse.get(i));
-		}
-	}
-	
-	static void find(int x,int y) {
-		visit[x][y] = true;
-		count++;
-		
-		for(int i=0;i<4;i++) {
-			int cx = x+dx[i];
-			int cy = y+dy[i];
-			if(cx>=0 && cy>=0 && cx<n && cy<n) {
-				if(visit[cx][cy]==false && arr[cx][cy]==1) {				
-					find(cx,cy);	
-				}
-			}
-		}
-	}
+    static int[][] map;
+    static int n;
+    static Queue<int[]> queue = new LinkedList<>();
+    static boolean[][] visited;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    static ArrayList<Integer> count = new ArrayList<>();
+    static int houses=0, house=0;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        map = new int[n][n];
+        visited = new boolean[n][n];
+        for(int i=0; i<n; i++){
+            String line = br.readLine();
+            for(int j=0; j<n; j++){
+                map[i][j] = line.charAt(j)-'0';
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++) {
+                if (map[i][j]==1 && !visited[i][j]){
+                    house=1;
+                    bfs(i,j);
+                    houses++;
+                    count.add(house);
+                }
+            }
+        }
+
+        Collections.sort(count);
+
+        System.out.println(houses);
+        for(int i:count){
+            System.out.println(i);
+        }
+    }
+
+    static void bfs(int x, int y){
+        queue.add(new int[] {x,y});
+        visited[x][y]=true;
+        int nx=0, ny = 0;
+
+        while(!queue.isEmpty()){
+            int[] temp = queue.poll();
+            int cx = temp[0];
+            int cy = temp[1];
+
+            for(int i=0; i<4; i++){
+                nx = cx+ dx[i];
+                ny = cy+ dy[i];
+                if(nx>=0 && ny>=0 && nx<n && ny<n){
+                    if(!visited[nx][ny] && map[nx][ny]==1){
+                        visited[nx][ny]=true;
+                        house++;
+                        queue.add(new int[] {nx,ny});
+                    }
+
+                }
+            }
+        }
+    }
 }
