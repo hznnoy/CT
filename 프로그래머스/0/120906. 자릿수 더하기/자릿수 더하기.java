92f8a0b7250720336ba19392a9932a9928cb3d73
hznnoy <152006906+hznnoy@users.noreply.github.com>
2025-07-31 12:31:15 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 88 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String str = Integer.toString(n);
+        int len = str.length();
+        
+        for(int i=0; i<len; i++){
+            answer += str.charAt(i)-'0';
+        }
+        return answer;
+    }
+}
