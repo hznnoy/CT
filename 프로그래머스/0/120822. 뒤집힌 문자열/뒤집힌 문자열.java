1d5abc84bdd848597f608c17fe9b0d2e72455d38
hznnoy <152006906+hznnoy@users.noreply.github.com>
2025-07-31 12:16:12 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.05 ms, Memory: 89.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        sb.append(my_string);
+        sb.reverse();
+        
+        answer = sb.toString();
+        return answer;
+    }
+}
