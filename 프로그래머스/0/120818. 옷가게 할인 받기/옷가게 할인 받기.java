d003dfba8e06dd88272efdc88b09e757c9eaade8
hznnoy <152006906+hznnoy@users.noreply.github.com>
2025-08-04 11:08:33 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.39 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+
+class Solution {
+    public double solution(int price) {
+        double answer = 0;        
+        
+        if(price>=100000 && price<300000){
+            answer = price*0.95;
+        }else if(price>=300000 && price<500000){
+            answer = price*0.9;
+        }else if (price>=500000){
+            answer = price*0.8;
+        }else{
+            answer = price;
+        }
+        return Math.floor(answer);
+    }
+}
