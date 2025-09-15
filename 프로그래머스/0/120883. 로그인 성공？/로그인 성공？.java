fc649ef013f6c249743ec04a185e88daf6da4fff
hznnoy <152006906+hznnoy@users.noreply.github.com>
2025-09-16 00:01:32 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -1,26 +1,15 @@
 class Solution {
     public String solution(String[] id_pw, String[][] db) {
         String answer = "";
-        boolean id = false;
-        boolean pw = false;
-        
-        for(int i=0; i<db.length; i++){
-            if(id_pw[0].equals(db[i][0]) && id_pw[1].equals(db[i][1])){
-                answer = "login"; 
-                return answer;
-            }else{
-                if(id_pw[0].equals(db[i][0])){
-                    id = true;
-                    if(!id_pw[1].equals(db[i][1])) answer = "wrong pw";
-                } else if(id_pw[0].equals(db[i][0]) && !id_pw[1].equals(db[i][1])){
-                    pw = true;
+
+        for(int i = 0; i < db.length; i++){
+            if(db[i][0].equals(id_pw[0])){
+                if(db[i][1].equals(id_pw[1])){
+                    return "login";
                 }
-            } 
+                return "wrong pw";
+            }
         }
-        
-        if(id==false) answer = "fail";
-        
-        
-        return answer;
+        return "fail";
     }
 }
