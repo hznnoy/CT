f91367a50f6df9fcbe6b94d2e1f18aa24449d817
hznnoy <152006906+hznnoy@users.noreply.github.com>
2025-10-16 23:43:18 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.30 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -0,0 +1,15 @@
+import java.util.StringTokenizer;
+
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        String s = my_string.replaceAll("[^0-9]", " ");
+        StringTokenizer st = new StringTokenizer(s, " ");
+
+        while (st.hasMoreTokens()) {
+            answer += Integer.parseInt(st.nextToken());
+        }
+
+        return answer;
+    }
+}
