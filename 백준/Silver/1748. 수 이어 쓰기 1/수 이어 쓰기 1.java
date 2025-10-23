33c88b045fc0520994d7646ac0fdcdabe77d5132
hznnoy <152006906+hznnoy@users.noreply.github.com>
2025-10-23 22:57:11 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 436 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int totalCount = 0;
+		int num = 1;
+		int criteria = 10;
+		for (int i = 1; i <= n; i++) {
+			if (i % criteria == 0) {
+				num += 1;
+				criteria *= 10;
+			}
+			totalCount += num;
+		}
+		System.out.println(totalCount);
+	}
+}
